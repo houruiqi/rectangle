62f38ccc535910c036484ce016898880f4ca511c
houruiqi <971469259@qq.com>
2020-04-06 22:26:10 +0800

    v

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,10 +18,37 @@ module.exports = function(grunt) {
                 eslintrc: './.eslintrc.json'
             },
             target: ['*.js']
-        }
+        },
+        mochacli: {
+            options: {
+              reporter: 'spec',
+              bail: true
+            },
+            all: ['test/*.js']
+          },
+          mocha_istanbul: {
+            coverage: {
+              src: 'test'
+            }
+          },
+          istanbul_check_coverage: {
+            default: {
+              options: {
+                coverageFolder: 'coverage*',
+                check: {
+                  lines: 90,
+                  statements: 90
+                }
+              }
+            }
+          }
     });
     grunt.loadNpmTasks('grunt-htmlhint');
     grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-eslint');
-    grunt.registerTask('default',['htmlhint','csslint','eslint']);
+    grunt.loadNpmTasks('grunt-mocha-cli');
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
+    grunt.registerTask('default',['htmlhint','csslint','eslint','mochacli']);
+    grunt.registerTask('cover', ['mocha_istanbul']);
+    grunt.registerTask('check-cover', ['istanbul_check_coverage']);
 };
